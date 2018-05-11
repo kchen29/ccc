b5e2a81e75de8cb5ec3a4f1e7a82fd451c24a207
kchen29 <kchen29@stuy.edu>
2018-05-11 12:15:39 -0400

    lexer

--- lexer.lisp
+++ lexer.lisp
@@ -1,12 +1,19 @@
-;;;;lexer
-
-(defun lexify (file)
-  "Given SPECS, classify each token in FILE, producing a token list."
+(defun lexify (file classifier)
+  "Given CLASSIFIER, classify each token in FILE, producing a token list."
   (with-open-file (stream file)
-    (do ((sub (read-token stream)
-              (read-token stream)))
-        ((string= "" sub))
-      (print sub))))
+    (loop for sub = (read-token stream)
+          until (string= "" sub)
+          collect (funcall classifier sub))))
+
+(defmacro do-stream ((var stream) (test &optional (result nil result-supplied-p)) &body body)
+  "Concise way to iterate over STREAM. Iterates over each character; stops when eof is found or
+   TEST is true, and returns RESULT. Performs BODY after each time VAR is set."
+  `(do ((,var (read-char ,stream nil nil)
+              (read-char ,stream nil nil)))
+       ,(if result-supplied-p
+            `((or (null ,var) ,test) ,result)
+            `((or (null ,var) ,test)))
+     ,@body))
 
 (defun read-token (stream)
   "Reads a token from STREAM."
@@ -18,16 +25,6 @@
     ;;then read token
     (do-stream (char stream) ((whitespace-p char))
       (princ char s-stream))))
-
-(defmacro do-stream ((var stream) (test &optional (result nil result-supplied-p)) &body body)
-  "Concise way to iterate over STREAM. Iterates over each character; stops when eof is found or
-   TEST is true, and returns RESULT. Performs BODY after each time VAR is set."
-  `(do ((,var (read-char ,stream nil nil)
-              (read-char ,stream nil nil)))
-       ,(if result-supplied-p
-            `((or (null ,var) ,test) ,result)
-            `((or (null ,var) ,test)))
-     ,@body))
 
 (defun whitespace-p (char)
   "Returns true if CHAR is a whitespace character."
